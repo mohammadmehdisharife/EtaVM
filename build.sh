8c8877ddac2395ef63cea4652dcc68f4ac9f6805
mohammadmehdisharife <mohammadmehdisharife@gmail.com>
2025-12-01 20:03:02 +0330

    win32 and win64 build with mingw

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
-gcc csxl.c function.c -o csxl
+mkdir build
+
+# linux
+gcc csxl.c function.c -o ./build/csxl
+
+# win64
+x86_64-w64-mingw32-gcc csxl.c function.c -o ./build/csxl_win64.exe
+
+# win32
+i686-w64-mingw32-gcc csxl.c function.c -o ./build/csxl_win32.exe
